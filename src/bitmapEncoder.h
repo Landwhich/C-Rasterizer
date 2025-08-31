41562df25aba1e3be5b1d17b14cff773bc81f629
Landwhich <will.f.richards@gmail.com>
2025-08-30 23:56:46 -0400

    updated gitignore properly

--- src/bitmapEncoder.h
+++ src/bitmapEncoder.h
@@ -1,7 +1,8 @@
+#pragma once
+
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
 #include "dataTypes.c"
-#pragma once
 
 void createBMP(const char *filename, const int width, const int height, float3** imgData);
